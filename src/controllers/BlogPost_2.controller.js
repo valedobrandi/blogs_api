5649d38031d164205d42439c75979865bbb1d6f3
valedobrandi <valedobrandi@gmail.com>
2024-06-21 14:01:56 -0300

    requerimento 18

--- src/controllers/BlogPost_2.controller.js
+++ src/controllers/BlogPost_2.controller.js
@@ -20,20 +20,16 @@ const deleteById = async (req, res, next) => {
   }
 };
 
-const isQuery = (query) => (query.q && query.q !== '');
-const isQueryEmpty = (query) => (query.q && query.q === '');
 const findByQuery = async (req, res, next) => {
+  const isQueryEmpty = (query) => (query.q && query.q === '');
   try {
-    if (isQuery) {
-      const { status, data } = await blogsPost2Service
-        .findByQuery(req.query.q);
-      return res.status(mapStatusHTTP(status)).json(data);
-    }
     if (isQueryEmpty(req.query)) {
       const { status, data } = await blogsPostService.getAll();
       return res.status(mapStatusHTTP(status)).json(data);
     }
-    return res.status(200).json([]);
+    const { status, data } = await blogsPost2Service
+      .findByQuery(req.query.q);
+    return res.status(mapStatusHTTP(status)).json(data);
   } catch (error) {
     next(error);
   }
